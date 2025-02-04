e2a7a3f80d910159a0aebfcab0973262d89f1fec
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-04 19:55:04 +0530

    mobile notification-(if/else) implemented

--- KotlinFundamentals/src/main/kotlin/Main.kt
+++ KotlinFundamentals/src/main/kotlin/Main.kt
@@ -3,5 +3,14 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
 
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(notification: Int) {
+    if (notification < 100) println("You have $notification notifications")
+    else println("Your phone is blowing up! You have 99+ notifications.")
 }
