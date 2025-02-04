7d57b67108a384fcd6e5f2eeab83d6c5f7b7d58a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-04 20:41:31 +0530

    applied elvis operator

--- KotlinFundamentals/src/main/kotlin/Main.kt
+++ KotlinFundamentals/src/main/kotlin/Main.kt
@@ -13,13 +13,22 @@ fun main() {
         title = "Song name",
         artist = "K K",
         1999,
-        playCount = 10,
-        isPopular = false
+        playCount = 10
     )
     songCatalog.printDescription()
+
+    val winningBid = Bid(5000, "Private Collector")
+    println("Item A is sold at ${auctionPrice(winningBid, 2000)}.")
+    println("Item B is sold at ${auctionPrice(null, 3000)}.")
 }
 
 fun printNotificationSummary(notification: Int) {
     if (notification < 100) println("You have $notification notifications")
     else println("Your phone is blowing up! You have 99+ notifications.")
+}
+
+class Bid(val amount: Int, val bidder: String)
+
+fun auctionPrice(bid: Bid?, minimumPrice: Int): Int {
+    return bid?.amount ?: minimumPrice
 }
--- KotlinFundamentals/src/main/kotlin/SongCatalog.kt
+++ KotlinFundamentals/src/main/kotlin/SongCatalog.kt
@@ -4,10 +4,12 @@ class SongCatalog(
     val title: String,
     val artist: String,
     val year: Int,
-    val playCount: Int,
-    val isPopular: Boolean
+    val playCount: Int
 ) {
+    val isPopular: Boolean
+        get() = this.playCount >= 1000
+
     fun printDescription() {
-        println("$title, performed by $artist, was released in $year.")
+        println("$title, performed by $artist, was released in $year. - isPopular: $isPopular")
     }
 }
