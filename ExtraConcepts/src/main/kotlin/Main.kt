164ed45bb142f565f560b0c0ba02d908afa43dfe
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-02-23 12:07:54 +0530

    null safety implemented

--- ExtraConcepts/src/main/kotlin/Main.kt
+++ ExtraConcepts/src/main/kotlin/Main.kt
@@ -1,4 +1,16 @@
 package org.example
 
 fun main() {
+    //null safety------------------------------------
+    println("Null safety----")
+    var s: String? = null
+    println(s?.length ?: 10) //elvis operator
+    s?.let {                   //let
+        println(it)
+    }
+    val obj: Any = "Hello World"
+    val str: String? = obj as? String
+    val num: Int? = obj as? Int
+    println("Safe casts: $str, $num")
+
 }
